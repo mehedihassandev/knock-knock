0eac5c5837088fca182145f7cc5f46fbd8d855f6
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-04-26 20:29:04 +0600

    Create API slice

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./apiSlice";
+import authSliceReducer from "./auth/authSlice";
+import conversationsSliceReducer from "./conversations/conversationsSlice";
+import messagesSliceReducer from "./messages/messagesSlice";
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSliceReducer,
+    conversations: conversationsSliceReducer,
+    messages: messagesSliceReducer,
+  },
+
+  devTools: !process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
