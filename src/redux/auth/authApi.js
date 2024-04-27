030432f46caae46068015259bb5b9d7accbde6a4
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-04-27 11:56:31 +0600

    Create auth api endpoint

--- src/redux/auth/authApi.js
+++ src/redux/auth/authApi.js
@@ -1,7 +1,54 @@
 import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from "./authSlice";
 
 export const authApi = apiSlice.injectEndpoints({
-  endpoints: (builder) => {
-    // endpoints here
-  },
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: "/register",
+        method: "POST",
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+
+          localStorage.setItem(
+            "auth",
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+
+          dispatch(userLoggedIn(result));
+        } catch (err) {}
+      },
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: "/login",
+        method: "POST",
+        body: data,
+      }),
+
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+
+          localStorage.setItem(
+            "auth",
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+
+          dispatch(userLoggedIn(result));
+        } catch (err) {}
+      },
+    }),
+  }),
 });
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
